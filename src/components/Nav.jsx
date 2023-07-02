6a1b50ddefb1454cba35505e374c7e5d26eb7d27
Prachi-2001 <prachipachang164@gmail.com>
2023-07-02 16:23:48 +0530

    inital commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div style={{minWidth:'100vw'}}>
+      <nav class="navbar navbar-light fixed-top" style={{backgroundColor:"#9505E3"}}>
+        <div class="container">
+          <Link class="navbar-brand" to="/">
+            <img
+              src="./letter-craft logo.png"
+              alt="Logo"
+              width="150"
+              height="24"
+            />      
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
